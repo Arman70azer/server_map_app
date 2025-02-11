9c9dcd235fd90ba25cc44275ff4f178f0e95969a
aauvray <auvrayarman@gmail.com>
2025-02-11 16:59:04 +0100

    email

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -3,20 +3,20 @@ const db = require("../../db.json"); // Chargement de la base de données JSON
 
 module.exports = {
   authenticateUser: function (req, res) {
-    const { id_token } = req.body;
+    const { email } = req.body;
 
-    if (!id_token) {
+    if (!email) {
       return res.status(400).json({ message: "ID token is required" });
     }
 
     // Simuler la validation du token
-    const user = db.users.find(u => u.id_token === id_token);
+    const user = db.users.find(u => u.email === email);
     if (!user) {
       return res.status(401).json({ message: "Invalid ID token" });
     }
 
     // Générer un token d'accès factice
-    const access_token = "fake-jwt-token-for-" + user.id;
+    const access_token = "token" + user.id;
     return res.status(200).json({ access_token });
   }
 };
